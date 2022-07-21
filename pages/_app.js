d2bc9fda6a3dbb7b502ab899e2b335669bfa4598
akhil <iamakhilbg@gmail.com>
2022-07-21 10:39:51 +0530

    Baseweb added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
 
+import { BaseProvider, LightTheme } from 'baseui'
+
 import { DefaultSeo } from 'next-seo'
 import Head from 'next/head'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import SEO from '../next-seo.config'
+import { Client as Styletron } from 'styletron-engine-atomic'
+import { Provider as StyletronProvider } from 'styletron-react'
+import { styletron } from '../styletron'
 
+//
+// const engine = new Styletron()
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -12,7 +21,11 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
+      <StyletronProvider value={styletron}>
+        <BaseProvider theme={LightTheme}>
+          <Component {...pageProps} />
+        </BaseProvider>
+      </StyletronProvider>
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import { NextSeo } from 'next-seo'
+import Select from './Components/Select'
 
 export default function Home() {
   return (
     <>
       <NextSeo title="Simple Usage Example" />
-      <main className="container"></main>
+      <main className="container">
+        <Select />
+      </main>
     </>
   )
 }
